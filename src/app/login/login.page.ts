2bd1eaf6ba6dbc341f85d9aa3e4e8828b805306d
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-09-04 17:37:00 -0300

    Página login / Router y AuthService

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service'; 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  login() {
+
+    // Autenticación exitosa, establecer el nombre de usuario en el servicio
+    this.authService.setUsername(this.username);
+
+    // Redirigir a la página home
+    this.router.navigate(['/home']);
+  }
+
+
+}
