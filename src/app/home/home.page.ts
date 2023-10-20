140a61599faf87929439b03634ff6f46f011ad0e
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-10-19 22:34:55 -0300

    Cerrar sesión

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -51,7 +51,10 @@ export class HomePage implements OnInit {
     }
   }
 
-  listaUsuarios() {
-    this.router.navigate(['/lista-usuarios']);
+  cerrarSesion() {
+    // Realiza las acciones necesarias para cerrar la sesión, como eliminar tokens de autenticación o borrar datos de usuario en el cliente.
+
+    // Redirige al usuario a la página de inicio de sesión (login)
+    this.router.navigate(['/login']);
   }
 }
