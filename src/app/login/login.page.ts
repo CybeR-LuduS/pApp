533c18fa74d9b69273a7e596704e07e9a57979a9
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-10-19 22:21:12 -0300

    finalización autenticación login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,10 +11,11 @@ import { HttpClient } from '@angular/common/http';
 export class LoginPage {
   username: string = '';
   password: string = '';
+  errorMessage: string = ''; // Variable para almacenar el mensaje de error
+  errorMessageVisible: boolean = false; // Variable para controlar la visibilidad del mensaje de error
 
   constructor(
     private router: Router,
-    private authService: AuthService,
     private http: HttpClient
   ) {}
 
@@ -26,8 +27,6 @@ export class LoginPage {
       },
     };
 
-    // Almacenar el nombre de usuario en el servicio de autenticación
-    this.authService.setUsername(this.username);
 
     // Realizar una solicitud GET a la API de Django para validar el inicio de sesión
     const apiUrl = `http://127.0.0.1:8000/api/lista_usuarios`;
@@ -41,8 +40,14 @@ export class LoginPage {
           // Inicio de sesión válido, puedes redirigir a la página de inicio o realizar otras acciones
           this.router.navigate(['/home'], navigationExtras);
         } else {
-          // Inicio de sesión no válido, muestra un mensaje de error al usuario
-          console.log('Credenciales inválidas (usuario y/o contraseña');
+          // Inicio de sesión no válido, establece el mensaje de error y muestra el mensaje
+          this.errorMessage = 'Credenciales inválidas (usuario y/o contraseña)';
+          this.errorMessageVisible = true;
+
+          // Ocultar el mensaje de error después de 3 segundos
+          setTimeout(() => {
+            this.errorMessageVisible = false;
+          }, 3000); // 3000 milisegundos (3 segundos)          
         }
       },
       (error) => {
