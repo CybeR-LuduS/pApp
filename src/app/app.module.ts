1a35a4ab8c70fea4aed07e9d44fca598983c44f9
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-11-11 14:01:48 -0300

    Ionic Build, cap IOS, cap android, emailComposer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, FormsModule, 
             MatIconModule, MatButtonModule, MatToolbarModule, MatProgressBarModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    EmailComposer,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
