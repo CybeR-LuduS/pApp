e4fa38e40065c6148dd6304b4ae2bdfe9d22c902
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-11-19 20:28:09 -0300

    Definición de algunas pruebas unitarias

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,26 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 import { LoginPage } from './login.page';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginPage],
-      imports: [IonicModule.forRoot(), HttpClientModule], 
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      imports: [IonicModule.forRoot(), RouterTestingModule, HttpClientModule, FormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call the login method', async () => {
+    spyOn(component, 'login');
+    await fixture.whenStable();
+    const buttons = el.queryAll(By.css('ion-button'));
+    const loginButton = buttons.find(button => button.nativeElement.textContent.trim() === 'Ingresar');
+    if (loginButton) {
+      loginButton.triggerEventHandler('click', null);
+      expect(component.login).toHaveBeenCalledTimes(1);
+    } else {
+      fail('No se encontró el botón de inicio de sesión');
+    }
+  });
+
 });
--- src/app/recuperar/recuperar.page.spec.ts
+++ src/app/recuperar/recuperar.page.spec.ts
@@ -3,24 +3,51 @@ import { IonicModule } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RecuperarPage } from './recuperar.page';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 describe('RecuperarPage', () => {
   let component: RecuperarPage;
   let fixture: ComponentFixture<RecuperarPage>;
+  let el: DebugElement;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RecuperarPage],
       imports: [IonicModule.forRoot(), HttpClientModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: Router, useValue: { navigate: jasmine.createSpy('navigate') } },
+        { provide: AuthService, useValue: { setUsername: jasmine.createSpy('setUsername') } }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(RecuperarPage);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the login method', () => {
+    spyOn(component, 'login');
+    el.query(By.css('ion-button')).triggerEventHandler('click', null);
+    expect(component.login).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set username in AuthService and navigate to /login', () => {
+    component.email = 'test@example.com';
+    component.login();
+    expect(authService.setUsername).toHaveBeenCalledWith('test@example.com');
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
