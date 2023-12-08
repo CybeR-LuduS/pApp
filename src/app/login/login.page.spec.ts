d4135540b2237601ca96e54cce6df63da003bf8e
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2023-12-08 13:46:07 -0300

    Corrección y mejoras de pruebas unitarias

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -29,16 +29,53 @@ describe('LoginPage', () => {
   });
 
   it('debería llamar al método de inicio de sesión', async () => {
-    spyOn(component, 'login');
+    let loginCalled = false;
+    component.login = () => {
+      loginCalled = true;
+    };
     await fixture.whenStable();
     const buttons = el.queryAll(By.css('ion-button'));
     const loginButton = buttons.find(button => button.nativeElement.textContent.trim() === 'Ingresar');
     if (loginButton) {
       loginButton.triggerEventHandler('click', null);
-      expect(component.login).toHaveBeenCalledTimes(1);
+      expect(loginCalled).toBe(true);
     } else {
       fail('No se encontró el botón de inicio de sesión');
     }
+  });
+
+  it('debería tener campos de entrada para el nombre de usuario y la contraseña', () => {
+    const usernameInput = el.query(By.css('ion-input[data-cy=username-input]'));
+    const passwordInput = el.query(By.css('ion-input[data-cy=password-input]'));
+    expect(usernameInput).toBeTruthy();
+    expect(passwordInput).toBeTruthy();
+  });
+
+  it('debería limpiar los valores de username y password', () => {
+    
+    component.username = 'test@example.com';
+    component.password = 'password123';
+  
+   
+    component.limpiarCampos();
+  
+    
+    expect(component.username).toEqual('');
+    expect(component.password).toEqual('');
+  });
+
+  it('debería tener un enlace para recuperar la cuenta', () => {
+    const recoverLink = el.query(By.css('a[routerLink="/recuperar"]'));
+    expect(recoverLink).toBeTruthy();
+  });
+
+  it('debería mostrar un mensaje de error cuando el inicio de sesión falla', () => {
+    component.errorMessage = 'Error de inicio de sesión';
+    component.errorMessageVisible = true;
+    fixture.detectChanges();
+    const errorMessage = el.query(By.css('.error-message:not(.hidden)'));
+    expect(errorMessage).toBeTruthy();
+    expect(errorMessage.nativeElement.textContent.trim()).toBe('Error de inicio de sesión');
   });
 
 });
